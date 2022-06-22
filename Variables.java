fc02b892ceed48167bb7cd10e42f50b8cb8a8771
AADITYAGHOSH <77581634+AADITYAGHOSH@users.noreply.github.com>
2022-06-22 23:38:38 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+package com.example.comments;
+
+public class Variables {
+    public static void main(String[]args){
+        String name="JOHN";
+        int num=22;
+        float num1=22.22f;
+        boolean ans=true;
+        char alp='A';
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(ans);
+        System.out.println(alp);
+    }
+}
