1ca82864d8e827c6b7af77d323738ffba871b1ee
AADITYAGHOSH <77581634+AADITYAGHOSH@users.noreply.github.com>
2022-07-07 16:11:16 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,10 @@
+public class If {
+    public static void main(String[]args){
+        int x=33;
+        if(x%3==0){
+            System.out.println("x is divisible by 3");
+        } else{
+            System.out.println("x is not divisible by 3");
+        }
+    }
+}
