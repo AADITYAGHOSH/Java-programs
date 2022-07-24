622c00c0b0e144431358508f0f460bd53fd35909
AADITYAGHOSH <77581634+AADITYAGHOSH@users.noreply.github.com>
2022-07-24 16:41:17 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[]args){
+        int[]ages={1,2,3,4,5};
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+
+    }
+}
