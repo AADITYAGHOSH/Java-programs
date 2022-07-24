d5437eabba2146e2d4d57fa4bf1c04543ac8e085
AADITYAGHOSH <77581634+AADITYAGHOSH@users.noreply.github.com>
2022-07-24 17:11:55 +0530

    Add files via upload

---
+++ Arraylooping.java
@@ -0,0 +1,9 @@
+public class Arraylooping {
+    public static void main(String[]args){
+        int[]age={1,2,3,4,5};
+        System.out.println("Using for loop");
+        for(int i=0;i<= age.length;++i){
+            System.out.println(age[i]);
+        }
+    }
+}
